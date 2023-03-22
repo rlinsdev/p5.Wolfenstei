dda8fefef8dbd67144d69ec188b295c3b2b32dc4
Rodrigo Lins <rlinsdev@gmail.com>
2023-03-22 10:30:39 -0300

    Steps and distToSide s

--- sketch.js
+++ sketch.js
@@ -1,65 +1,92 @@
 // Map
 const gamemap = [
-	[1,1,1,1,1,1,1,1,1,1],
-	[1,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,1],
-	[1,1,1,1,1,1,1,1,1,1]
-  ];
+	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
 
-  // Possition and direction of player
-  let pos;
-  let dir;
+// Possition and direction of player
+let pos;
+let dir;
 
-  let cameraPlane;
+let cameraPlane;
 
-  // size of screen
-  const gameWidth = 320; //=> This will be the base to calculate the vectors in rays
-  const gameHeight = 200;
+// size of screen
+const gameWidth = 320; //=> This will be the base to calculate the vectors in rays
+const gameHeight = 200;
 
-  function setup() {
+function setup() {
 	// Size of screen. This will generate var height and width globaly
 	createCanvas(gameWidth, gameHeight);
 	// remove black row to divide frames
 	noStroke();
 
-	pos = createVector(5,5);
-	dir = createVector(0,-1);
-	cameraPlane = createVector(0.66,0);
-  }
+	pos = createVector(5, 5);
+	dir = createVector(0, -1);
+	cameraPlane = createVector(0.66, 0);
+}
 
-  /*This is the function with a ethernal loop*/
-  function draw() {
+/*This is the function with a eternal loop*/
+function draw() {
 	// ceiling
-	background(190,190,255);
+	background(190, 190, 255);
 
 	fill(130);
 
 	// Draw the floor
-	rect(0,height/2, width, height/2);
+	rect(0, height / 2, width, height / 2);
 
-	for (let pixel = 0;pixel < width; pixel++) {
-	  //Formula to get all the pixels in the all width, range from -1 until 1 (range of POV)
-	  const multiplier = 2 * (pixel/width) -1;
-	  // New Small peace of pixel in the range of POV (A slice of the full plane )
-	  const cameraPixel = p5.Vector.mult(cameraPlane, multiplier);
-	  // Ray from the Player until the small peace of plane
-	  const rayDir = p5.Vector.add(dir, cameraPixel);
+	for (let pixel = 0; pixel < width; pixel++) {
+		//Formula to get all the pixels in the all width, range from -1 until 1 (range of POV)
+		const multiplier = 2 * (pixel / width) - 1;
+		// New Small peace of pixel in the range of POV (A slice of the full plane )
+		const cameraPixel = p5.Vector.mult(cameraPlane, multiplier);
+		// Ray from the Player until the small peace of plane
+		const rayDir = p5.Vector.add(dir, cameraPixel);
 
-	  // .mag will calculate magnitude of vector
-	  const deltaDistX = rayDir.mag()/rayDir.x
-	  const deltaDistY = rayDir.mag()/rayDir.y
+		// .mag will calculate magnitude of vector
+		const deltaDistX = rayDir.mag() / rayDir.x
+		const deltaDistY = rayDir.mag() / rayDir.y
 
-	  // Getting the position of player, but without decimal places
-	  const mapPos = createVector(floor(pos.x), floor(pos.y));
+		// Getting the position of player, but without decimal places
+		const mapPos = createVector(floor(pos.x), floor(pos.y));
+
+		let distToSideX;
+		let distToSideY;
+
+		let stepY;
+		let stepX;
+
+		// Rays (FOV) going to left (X)
+		if (rayDir.x < 0) {
+			distToSideX = (pos.x - mapPos.x) * deltaDistX;
+			stepX = -1;
+		} else {
+			// Going Right
+			distToSideX = (mapPos.x + 1 - pos.x) * deltaDistX;
+			stepX = 1;
+		}
+
+		// Rays (FOV) going to Up (Y)
+		if (rayDir.y < 0) {
+			distToSideY = (pos.y - mapPos.y) * deltaDistY;
+			stepY = -1;
+		} else {
+			// Going Down
+			distToSideY = (mapPos.y + 1 - pos.y) * deltaDistY;
+			stepY = 1;
+		}
+
 
 
 
 
 	}
-  }
+}
