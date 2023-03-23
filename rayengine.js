e674c0ee65d0ecf60c89233873b6fa92569cd1d4
Rodrigo Lins <rlinsdev@gmail.com>
2023-03-23 17:28:54 -0300

    Refactoring code

---
+++ rayengine.js
@@ -0,0 +1,146 @@
+
+class RayEngine {
+	constructor(w, h, map) {
+		this.width = w;
+		this.height = h;
+		this.map = map.map;
+
+		this.frameBuffer = createGraphics(this.width, this.height);
+
+		this.pos = createVector(5, 5);
+		this.dir = createVector(0, -1); // Dir normalized to 1. Get ease in math calculation
+		this.cameraPlane = createVector(0.66, 0);
+
+		this.multiplier = null;
+		this.cameraPixel = null;
+		this.rayDir = null;
+
+		this.deltaDistX = null;
+		this.deltaDistY = null;
+		this.distToSideX = null;
+		this.distToSideY = null;
+		this.stepY = null;
+		this.stepX = null;
+		this.mapPos = null;
+
+		this.hitSide = null;
+		this.wallHitMapPos = null;
+		this.perpendicularDist = null;
+		this.wallColor = null;
+	}
+
+	generateFrame() {
+		// dir.rotate(0.02);
+		// cameraPlane.rotate(0.02);
+
+		// ceiling
+		this.frameBuffer.background(190, 190, 255);
+		this.frameBuffer.fill(130);
+
+		// Draw the floor
+		this.frameBuffer.rect(0, this.height / 2, this.width, this.height / 2);
+
+		for (let pixel = 0; pixel < this.width; pixel++) {
+			this.calculateCurrentRay(pixel);
+
+			// Getting the position of player, but without decimal places
+			this.mapPos = createVector(floor(this.pos.x), floor(this.pos.y));
+
+			this.calculateDDAVariables();
+			this.performDDA();
+			this.calculatePerpendicularDist();
+			this.calculateWallColor();
+			this.drawVerticalLine(pixel);
+		}
+	}
+
+	calculateCurrentRay(xpos) {
+
+		//Formula to get all the pixels in the all width, range from -1 until 1 (range of POV)
+		this.multiplier = 2 * (xpos / this.width) - 1;
+		// New Small peace of pixel in the range of POV (A slice of the full plane )
+		this.cameraPixel = p5.Vector.mult(this.cameraPlane, this.multiplier);
+		// Ray from the Player until the small peace of plane
+		this.rayDir = p5.Vector.add(this.dir, this.cameraPixel);
+	}
+
+	calculateDDAVariables() {
+		this.deltaDistY = abs(1 / this.rayDir.y);
+		this.deltaDistX = abs(1 / this.rayDir.x);
+
+		// Rays (FOV) going to left (X)
+		if (this.rayDir.x < 0) {
+			this.distToSideX = (this.pos.x - this.mapPos.x) * this.deltaDistX;
+			this.stepX = -1;
+		} else {
+			// Going Right
+			this.distToSideX = (this.mapPos.x + 1 - this.pos.x) * this.deltaDistX;
+			this.stepX = 1;
+		}
+
+		// Rays (FOV) going to Up (Y)
+		if (this.rayDir.y < 0) {
+			this.distToSideY = (this.pos.y - this.mapPos.y) * this.deltaDistY;
+			this.stepY = -1;
+		} else {
+			// Going Down
+			this.distToSideY = (this.mapPos.y + 1 - this.pos.y) * this.deltaDistY;
+			this.stepY = 1;
+		}
+	}
+
+	performDDA() {
+		let hit = false;
+
+		// Initialize LineSize with the distance to axis
+		let ddaLineSizeY = this.distToSideY;
+		let ddaLineSizeX = this.distToSideX;
+
+		// Copy vector pos to wallHitMapPos vector
+		this.wallHitMapPos = this.mapPos.copy();
+
+		// Implementing DDA algorithm
+		while (hit == false) {
+
+			if (ddaLineSizeX < ddaLineSizeY) {
+				this.wallHitMapPos.x += this.stepX;
+				ddaLineSizeX += this.deltaDistX;
+				this.hitSide = 0;
+			} else {
+				this.wallHitMapPos.y += this.stepY;
+				ddaLineSizeY += this.deltaDistY;
+				this.hitSide = 1;
+			}
+			// Check if hit the wall
+			if (this.map[this.wallHitMapPos.x][this.wallHitMapPos.y] > 0) {
+				hit = true;
+			}
+		}
+	}
+
+	calculatePerpendicularDist() {
+		if (this.hitSide == 0) {
+			this.perpendicularDist = abs(this.wallHitMapPos.x - this.pos.x + ((1-this.stepX)/2))/this.rayDir.x;
+		} else {
+			this.perpendicularDist = abs(this.wallHitMapPos.y - this.pos.y + ((1-this.stepY)/2))/this.rayDir.y;
+		}
+	}
+
+	calculateWallColor() {
+		this.wallColor = this.hitSide ? 255: 128;
+	}
+
+	drawVerticalLine(xpos) {
+		// Size of wall
+		let wallLineHeight = this.height / this.perpendicularDist;
+		let lineStartY = this.height/2 - wallLineHeight/2;
+		let lineEndY = this.height/2 + wallLineHeight/2;
+
+		// Change the wall color. If hit 1 side, 1 color, another side, another color
+		// let color = hitSide ? 255: 128;
+		// put the color
+		this.frameBuffer.stroke(this.wallColor, 0, 0);
+		// Draw the line (wall)
+		this.frameBuffer.line(xpos, lineStartY, xpos, lineEndY);
+	}
+}
--- sketch.js
+++ sketch.js
@@ -1,22 +1,12 @@
 // Map
-const gamemap = [
-	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[1, 1, 0, 0, 0, 1, 0, 0, 1, 1],
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-	[1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-	[1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
-	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-];
+let gamemap;
+
+let engine;
 
 // Possition and direction of player
-let pos;
-let dir;
-
-let cameraPlane;
+// let pos;
+// let dir;
+// let cameraPlane;
 
 let xoff = 0;
 let yoff = 0;
@@ -30,163 +20,20 @@ const gameHeight = 200;
 
 function setup() {
 	// Size of screen. This will generate var height and width globaly
-	createCanvas(gameWidth, gameHeight);
+	createCanvas(800, 600);
 	// remove black row to divide frames
 	noStroke();
 
-	pos = createVector(5, 5);
-	dir = createVector(0, -1); // Dir normalized to 1. Get ease in math calculation
-	cameraPlane = createVector(0.66, 0);
-
-	// // Rotate the vector to change the angle of view (test)
-	// dir.rotate(1);
-	// cameraPlane.rotate(1);
+	gamemap = new Map();
+	engine = new RayEngine(gameWidth, gameHeight, gamemap);
 }
 
 /*This is the function with a eternal loop*/
 function draw() {
 
-	// dir.rotate(0.02);
-	// cameraPlane.rotate(0.02);
+	// // ceiling
+	background(35);
 
-	// ceiling
-	background(190, 190, 255);
-
-	fill(130);
-
-	// Draw the floor
-	rect(0, height / 2, width, height / 2);
-
-	for (let pixel = 0; pixel < width; pixel++) {
-		//Formula to get all the pixels in the all width, range from -1 until 1 (range of POV)
-		const multiplier = 2 * (pixel / width) - 1;
-		// New Small peace of pixel in the range of POV (A slice of the full plane )
-		const cameraPixel = p5.Vector.mult(cameraPlane, multiplier);
-		// Ray from the Player until the small peace of plane
-		const rayDir = p5.Vector.add(dir, cameraPixel);
-
-		// .mag will calculate magnitude of vector. Must be abs. DeltaDist is always positive
-		// This is very cost calculation.
-		// const deltaDistY = abs(rayDir.mag() / rayDir.y);
-		// const deltaDistX = abs(rayDir.mag() / rayDir.x);
-
-		// Just must be the same proportion. Change the 'Magnitude' to 1
-		// rayDir maybe will be zero (0).
-		// const deltaDistY = abs(1 / rayDir.y);
-		// const deltaDistX = abs(1 / rayDir.x);
-
-		let deltaDistY;
-		let deltaDistX;
-
-		// To not divide by zero, force to Y always be less then X
-		if (rayDir.x == 0) {
-			deltaDistX = 1;
-			deltaDistY = 0;
-		}  else {
-			if (rayDir.y) {
-				deltaDistX = abs(1 / rayDir.x);
-			}
-		}
-
-		// To not divide by zero, force to X always be less then Y
-		if (rayDir.y == 0) {
-			deltaDistX = 0;
-			deltaDistY = 1;
-		}  else {
-			if (rayDir.x) {
-				deltaDistY = abs(1 / rayDir.y);
-			}
-		}
-
-
-		// Getting the position of player, but without decimal places
-		const mapPos = createVector(floor(pos.x), floor(pos.y));
-
-		let distToSideX;
-		let distToSideY;
-
-		let stepY;
-		let stepX;
-
-		let perpendicularDist;
-
-		// Rays (FOV) going to left (X)
-		if (rayDir.x < 0) {
-			distToSideX = (pos.x - mapPos.x) * deltaDistX;
-			stepX = -1;
-		} else {
-			// Going Right
-			distToSideX = (mapPos.x + 1 - pos.x) * deltaDistX;
-			stepX = 1;
-		}
-
-		// Rays (FOV) going to Up (Y)
-		if (rayDir.y < 0) {
-			distToSideY = (pos.y - mapPos.y) * deltaDistY;
-			stepY = -1;
-		} else {
-			// Going Down
-			distToSideY = (mapPos.y + 1 - pos.y) * deltaDistY;
-			stepY = 1;
-		}
-
-
-		let hit = false;
-		let hitSide;
-
-		// Initialize LineSize with the distance to axis
-		let ddaLineSizeX = distToSideX;
-		let ddaLineSizeY = distToSideY;
-
-		// Copy vector pos to wallMapPos vector
-		let wallMapPos = mapPos.copy();
-
-		// Implementing DDA algorithm
-		while (hit == false) {
-
-			if (ddaLineSizeX < ddaLineSizeY) {
-				wallMapPos.x += stepX;
-				ddaLineSizeX += deltaDistX;
-				hitSide = 0;
-			} else {
-				wallMapPos.y += stepY;
-				ddaLineSizeY += deltaDistY;
-				hitSide = 1;
-			}
-			// Check if hit the wall
-			if (gamemap[wallMapPos.x][wallMapPos.y] > 0) {
-				hit = true;
-			}
-		}
-		// Formula to avoid fish yes. Will calculate the distance player until wall
-		if (hitSide == 0) {
-			perpendicularDist = abs(wallMapPos.x - pos.x + ((1-stepX)/2))/rayDir.x;
-		} else {
-			perpendicularDist = abs(wallMapPos.y - pos.y + ((1-stepY)/2))/rayDir.y;
-		}
-
-		// Size of wall
-		let wallLineHeight = height/perpendicularDist;
-		let lineStartY = height/2 - wallLineHeight/2;
-		let lineEndY = height/2 + wallLineHeight/2;
-
-		// Change the wall color. If hit 1 side, 1 color, another side, another color
-		let color = hitSide ? 255: 128;
-		// put the color
-		stroke(color, 0, 0);
-		// Draw the line (wall)
-		line(pixel, lineStartY, pixel, lineEndY);
-	}
-
-	// Perlin noise
-	pos.x = noise(xoff)*8;
-	pos.y = noise(yoff+100)*8;
-	rotSpeed = noise(aoff+50)/60;
-
-	xoff+=0.005;
-	yoff+=0.005;
-	aoff+=0.01;
-
-	dir.rotate(rotSpeed);
-	cameraPlane.rotate(rotSpeed);
+	engine.generateFrame();
+	image(engine.frameBuffer, 0, 0);
 }
